79fa897571523f70797ec4fd019816a75f6c1bb9
rashad al shukh <rashad000089@gmail.com>
2024-07-07 23:08:25 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.table1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tESTINGDataSet = new WindowsFormsApp1.TESTINGDataSet();
+            this.tempdbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tempdbDataSet = new WindowsFormsApp1.tempdbDataSet();
+            this.table_1TableAdapter = new WindowsFormsApp1.TESTINGDataSetTableAdapters.Table_1TableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tESTINGDataSet1 = new WindowsFormsApp1.TESTINGDataSet1();
+            this.table1BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.table_1TableAdapter1 = new WindowsFormsApp1.TESTINGDataSet1TableAdapters.Table_1TableAdapter();
+            this.tESTINGDataSet2 = new WindowsFormsApp1.TESTINGDataSet();
+            this.tESTINGDataSet2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table1BindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.nAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bUNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tESTDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.testsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempdbDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempdbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table1BindingSource
+            // 
+            this.table1BindingSource.DataMember = "Table_1";
+            this.table1BindingSource.DataSource = this.tESTINGDataSet;
+            // 
+            // tESTINGDataSet
+            // 
+            this.tESTINGDataSet.DataSetName = "TESTINGDataSet";
+            this.tESTINGDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tempdbDataSetBindingSource
+            // 
+            this.tempdbDataSetBindingSource.DataSource = this.tempdbDataSet;
+            this.tempdbDataSetBindingSource.Position = 0;
+            // 
+            // tempdbDataSet
+            // 
+            this.tempdbDataSet.DataSetName = "tempdbDataSet";
+            this.tempdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table_1TableAdapter
+            // 
+            this.table_1TableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nAMEDataGridViewTextBoxColumn,
+            this.bUNDataGridViewTextBoxColumn,
+            this.tESTDataGridViewTextBoxColumn,
+            this.testsDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.table1BindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(485, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tESTINGDataSet1
+            // 
+            this.tESTINGDataSet1.DataSetName = "TESTINGDataSet1";
+            this.tESTINGDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table1BindingSource1
+            // 
+            this.table1BindingSource1.DataMember = "Table_1";
+            this.table1BindingSource1.DataSource = this.tESTINGDataSet1;
+            // 
+            // table_1TableAdapter1
+            // 
+            this.table_1TableAdapter1.ClearBeforeFill = true;
+            // 
+            // tESTINGDataSet2
+            // 
+            this.tESTINGDataSet2.DataSetName = "TESTINGDataSet";
+            this.tESTINGDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tESTINGDataSet2BindingSource
+            // 
+            this.tESTINGDataSet2BindingSource.DataSource = this.tESTINGDataSet2;
+            this.tESTINGDataSet2BindingSource.Position = 0;
+            // 
+            // table1BindingSource2
+            // 
+            this.table1BindingSource2.DataMember = "Table_1";
+            this.table1BindingSource2.DataSource = this.tESTINGDataSet2BindingSource;
+            // 
+            // nAMEDataGridViewTextBoxColumn
+            // 
+            this.nAMEDataGridViewTextBoxColumn.DataPropertyName = "NAME";
+            this.nAMEDataGridViewTextBoxColumn.HeaderText = "NAME";
+            this.nAMEDataGridViewTextBoxColumn.Name = "nAMEDataGridViewTextBoxColumn";
+            // 
+            // bUNDataGridViewTextBoxColumn
+            // 
+            this.bUNDataGridViewTextBoxColumn.DataPropertyName = "BUN";
+            this.bUNDataGridViewTextBoxColumn.HeaderText = "BUN";
+            this.bUNDataGridViewTextBoxColumn.Name = "bUNDataGridViewTextBoxColumn";
+            // 
+            // tESTDataGridViewTextBoxColumn
+            // 
+            this.tESTDataGridViewTextBoxColumn.DataPropertyName = "TEST";
+            this.tESTDataGridViewTextBoxColumn.HeaderText = "TEST";
+            this.tESTDataGridViewTextBoxColumn.Name = "tESTDataGridViewTextBoxColumn";
+            // 
+            // testsDataGridViewTextBoxColumn
+            // 
+            this.testsDataGridViewTextBoxColumn.DataPropertyName = "tests";
+            this.testsDataGridViewTextBoxColumn.HeaderText = "tests";
+            this.testsDataGridViewTextBoxColumn.Name = "testsDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempdbDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempdbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTINGDataSet2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource tempdbDataSetBindingSource;
+        private tempdbDataSet tempdbDataSet;
+        private TESTINGDataSet tESTINGDataSet;
+        private System.Windows.Forms.BindingSource table1BindingSource;
+        private TESTINGDataSetTableAdapters.Table_1TableAdapter table_1TableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TESTINGDataSet1 tESTINGDataSet1;
+        private System.Windows.Forms.BindingSource table1BindingSource1;
+        private TESTINGDataSet1TableAdapters.Table_1TableAdapter table_1TableAdapter1;
+        private System.Windows.Forms.BindingSource tESTINGDataSet2BindingSource;
+        private TESTINGDataSet tESTINGDataSet2;
+        private System.Windows.Forms.BindingSource table1BindingSource2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nAMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bUNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tESTDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn testsDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.CellPainting += dataGridView1_CellPainting;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.table_1TableAdapter.Fill(this.tESTINGDataSet2.Table_1);
+            this.table_1TableAdapter1.Fill(this.tESTINGDataSet1.Table_1);
+            this.table_1TableAdapter.Fill(this.tESTINGDataSet.Table_1);
+
+            dataGridView1.CellPainting += dataGridView1_CellPainting;
+
+        }
+
+        public class CellTarget
+        {
+            public int RowIndex { get; set; }
+            public int ColumnIndex { get; set; }
+            public string TargetWord { get; set; }
+            public Color ShadowColor { get; set; }
+            public bool IsBold { get; set; }
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            // قائمة الخلايا المستهدفة والكلمات المستهدفة
+            List<CellTarget> targets = new List<CellTarget>
+    {
+        new CellTarget { RowIndex = 3, ColumnIndex = 3, TargetWord = "this", ShadowColor = Color.DarkCyan, IsBold = true },
+        new CellTarget { RowIndex = 3, ColumnIndex = 3, TargetWord = "text", ShadowColor = Color.Red, IsBold = true },
+        new CellTarget { RowIndex = 2, ColumnIndex = 2, TargetWord = "R", ShadowColor = Color.Red, IsBold = false },
+        new CellTarget { RowIndex = 1, ColumnIndex = 2, TargetWord = "R", ShadowColor = Color.Blue, IsBold = true }
+
+    };
+
+            foreach (var target in targets)
+            {
+                if (e.RowIndex == target.RowIndex && e.ColumnIndex == target.ColumnIndex && e.Value != null)
+                {
+                    string text = e.FormattedValue.ToString();
+                    if (text.Contains(target.TargetWord))
+                    {
+                        // رسم الخلفية الافتراضية للخلية
+                        e.PaintBackground(e.CellBounds, true);
+
+                        // إعدادات النص
+                        FontStyle fontStyle = target.IsBold ? FontStyle.Bold : FontStyle.Regular;
+                        Font textFont = new Font(e.CellStyle.Font, fontStyle);
+                        Color textColor = e.CellStyle.ForeColor;
+                        TextFormatFlags flags = TextFormatFlags.VerticalCenter | TextFormatFlags.Left;
+
+                        // إيجاد موضع الكلمة المستهدفة داخل النص
+                        int startIndex = text.IndexOf(target.TargetWord);
+                        if (startIndex >= 0)
+                        {
+                            // قياس النص قبل الكلمة المستهدفة
+                            string beforeWord = text.Substring(0, startIndex);
+                            Size beforeSize = TextRenderer.MeasureText(e.Graphics, beforeWord, textFont, e.CellBounds.Size, flags);
+
+                            // قياس الكلمة المستهدفة
+                            Size wordSize = TextRenderer.MeasureText(e.Graphics, target.TargetWord, textFont, e.CellBounds.Size, flags);
+
+                            // قياس النص بعد الكلمة المستهدفة
+                            string afterWord = text.Substring(startIndex + target.TargetWord.Length);
+                            Size afterSize = TextRenderer.MeasureText(e.Graphics, afterWord, textFont, e.CellBounds.Size, flags);
+
+                            // تحديد موضع النص داخل الخلية
+                            int textHeight = TextRenderer.MeasureText(e.Graphics, text, textFont, e.CellBounds.Size, flags).Height;
+                            Point textLocation = new Point(e.CellBounds.X + 2, e.CellBounds.Y + (e.CellBounds.Height - textHeight));
+
+                            // رسم النص قبل الكلمة المستهدفة
+                            TextRenderer.DrawText(e.Graphics, beforeWord, textFont, textLocation, textColor, flags);
+
+                            // رسم الظل
+                            Point shadowLocation = new Point(textLocation.X + beforeSize.Width + 2, textLocation.Y + 2);
+                            TextRenderer.DrawText(e.Graphics, target.TargetWord, textFont, shadowLocation, target.ShadowColor, flags);
+
+                            // رسم الكلمة المستهدفة
+                            Point wordLocation = new Point(textLocation.X + beforeSize.Width, textLocation.Y);
+                            TextRenderer.DrawText(e.Graphics, target.TargetWord, textFont, wordLocation, textColor, flags);
+
+                            // رسم النص بعد الكلمة المستهدفة
+                            Point afterLocation = new Point(wordLocation.X + wordSize.Width, wordLocation.Y);
+                            TextRenderer.DrawText(e.Graphics, afterWord, textFont, afterLocation, textColor, flags);
+                        }
+
+                        // منع الرسم الافتراضي
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+    }
+}
